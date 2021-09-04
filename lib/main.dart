318215841ba7c04c3a0d0a23a303381e1011feb0
Masayuki Hayashi <masayuki.hayashi.mcc@gmail.com>
2021-09-04 11:20:43 +0900

    それっぽいリスト表示にしてみた

--- lib/main.dart
+++ lib/main.dart
@@ -76,35 +76,23 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_entry',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text('ここにタイトルが入ります'),
+                  Text("本文\n本文\n"),
+                  Text('[2021-09-04 11:04:00]')
+                ],
+                crossAxisAlignment: CrossAxisAlignment.start,
+              )
+            )
+          );
+        },
+        itemCount: 2,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _createEntry,
